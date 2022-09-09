0be0f5fe972284cf2a79f61453e9a91bb49f35de
Hritikrohilla <90026084+TIG3R015@users.noreply.github.com>
2022-09-09 17:57:03 +0530

    Update mgos_ir.c

--- src/mgos_ir.c
+++ src/mgos_ir.c
@@ -10,6 +10,8 @@
 
 static IRAM void irrecv_nec_handler(int pin, void *arg)
 { 
+  void mgos_wdt_disable(void);
+  bool mgos_gpio_disable_int(int 5);
   
   struct mgos_irrecv_nec_s *obj = (struct mgos_irrecv_nec_s *)arg;
   // get microseconds
@@ -54,7 +56,7 @@ static IRAM void irrecv_nec_handler(int pin, void *arg)
       // report code
       // NO LOG or printf in ISR service routine, or in the handler
       // LOG(LL_DEBUG, ("IRRECV @ %d: %08X", pin, obj->code.dword));
-      bool mgos_gpio_disable_int(int 5);
+//       
       if (obj->handler) {
         obj->handler(obj->code.dword, obj->user_data);
       }
